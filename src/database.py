2d0c184f33ec179aa919f8aea047ba7616f819d8
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-06-12 11:33:05 -0700

    test_task_10_subclasshook

--- src/database.py
+++ src/database.py
@@ -14,9 +14,10 @@ def list_reminders():
         print()
 
 def add_reminder(text, date, ReminderClass):
-    if not issubclass(ReminderClass, DeadlinedReminder):
+    reminder = ReminderClass(text, date)    
+    if not isinstance(reminder, DeadlinedReminder):
         raise TypeError('Invalid Reminder Class')
-    reminder = ReminderClass(text, date)
+
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
         writer.writerow(reminder)
