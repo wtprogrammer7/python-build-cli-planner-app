2e0519dc2b933324da113dd0c3c573b34f8a2199
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-06-12 11:26:03 -0700

    test_task_9_accept_class

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from src.database import add_reminder, list_reminders
-
+from src.deadlined_reminders import DateReminder
 
 def handle_input():
     choice = input("Choice: ")
@@ -14,7 +14,7 @@ def handle_input():
         reminder = input("What would you like to be reminded about?: ")
         date = input("When is that due?: ")
 
-        add_reminder(reminder, date)
+        add_reminder(reminder, date, add_reminder())
         list_reminders()
     else:
         print("Invalid menu option")
--- src/database.py
+++ src/database.py
@@ -1,6 +1,5 @@
 import csv
-from src.reminder import PoliteReminder
-from src.deadlined_reminders import DateReminder
+from src.deadlined_reminders import DeadlinedReminder
 
 def list_reminders():
     f = open("reminders.csv", "r")
@@ -14,8 +13,10 @@ def list_reminders():
                 print(e.ljust(32), end=' ')
         print()
 
-def add_reminder(text, date):
-    reminder = DateReminder(text, date)
+def add_reminder(text, date, ReminderClass):
+    if not issubclass(ReminderClass, DeadlinedReminder):
+        raise TypeError('Invalid Reminder Class')
+    reminder = ReminderClass(text, date)
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
         writer.writerow(reminder)
