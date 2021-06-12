b6cc677f31e9ab8eb0be314bc53fccbac8c81f1b
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2021-06-12 10:45:09 -0700

    test_task_3_DeadlinedMetaReminder

---
+++ src/deadlined_reminders.py
@@ -0,0 +1,7 @@
+from abc import ABCMeta, abstractmethod
+from collections.abc import Iterable
+
+class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
+    @abstractmethod
+    def is_due(self):
+        pass
